2c617201b38fe0c13ce63be8de3ad23555dd595a
Lim JY <limjunyi03@gmail.com>
2023-06-08 06:25:45 +0000

    Simplify code for components

--- components.py
+++ components.py
@@ -7,33 +7,36 @@ def header(s: str):
     st.markdown('---')
 
 def emptylines(n: int) -> None:
+    '''Create n empty lines'''
 
-    for _ in range(n): st.write('')
+    for _ in range(n): 
+        st.write('')
 
-def searchbar():
+def searchbar() -> st.container():
+    '''Create a search bar'''
 
     with st.container():
-        
+
         col1, col2 = st.columns([6, 2])
         search_bar = col1.text_input('Search a keyword:', placeholder='Search')
-        filter_by = col2.selectbox('Sort by:', ['Most Popular', 'Price (Descending)', 'Price (Ascending)', 
+        filter_by = col2.selectbox('Sort by:', ['Most Popular', 
+                                                'Price (Descending)', 'Price (Ascending)', 
                                                 'Nights (Descending)', 'Nights (Ascending)'])
 
-        sort_by = {'by': ['number_of_reviews'], 'ascending': False} if filter_by == 'Most Popular' else \
-                  {'by': ['price'], 'ascending': False} if filter_by == 'Price (Descending)' else \
-                  {'by': ['price']} if filter_by == 'Price (Ascending)' else \
-                  {'by': ['minimum_nights'], 'ascending': False} if filter_by == 'Nights (Descending)' else \
-                  {'by': ['minimum_nights']}
+        sort_by = {'by': ['price' if 'Price' in filter_by else \
+                          'minimum_nights' if 'Nights' in filter_by else 'number_of_reviews'], 
+                   'ascending': 'Ascending' in filter_by}
 
     return search_bar, sort_by
 
-def display_result(query_result, initial=0, MAX_PER_PAGE=10):
+def display_result(query_result, initial=0, max_per_page=10):
+    '''Display query result in formatted layout'''
 
-    upper_bound = min(initial + MAX_PER_PAGE, len(query_result))
+    upper_bound = min(initial + max_per_page, len(query_result))
 
     st.write(f'Search result (Showing {initial + 1} - {upper_bound}) of {len(query_result)}')
 
-    for i in range(initial, min(initial + MAX_PER_PAGE, len(query_result))):
+    for i in range(initial, min(initial + max_per_page, len(query_result))):
 
         row = query_result.iloc[i]
 
