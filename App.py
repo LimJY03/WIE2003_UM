be8ada206a49e038f92befbf617ed49dba8a8966
Lim JY <limjunyi03@gmail.com>
2023-06-08 06:25:59 +0000

    Tidy up app

--- App.py
+++ App.py
@@ -1,42 +1,5 @@
-import pickle as pkl
 import streamlit as st
-import tensorflow as tf
-import pandas as pd
-from PIL import Image
-from components import searchbar, header, emptylines, display_result
-
-MAX_PER_PAGE = 10
-
-# Loading dataset
-df = pd.read_csv('./data/airbnb_data_clean.csv').iloc[:, 1:]
-
-# Loading models
-# nlp_model = 
-reg_model = pkl.load(open('./model/model.pkl', 'rb'))
-
-# Create pages
-def home():
-    header('Home')
-
-def about():
-    header('About')
-
-def booking():
-
-    header('Booking')
-
-    search, sort_by = searchbar()
-
-    query_result = df[df.apply(lambda row: search in row['name_of_listing'], axis=1)].sort_values(**sort_by)
-
-    emptylines(3)
-    display_result(query_result)
-
-def budgeting():
-
-    header('Budgeting')
-
-
+from pages import home, about, booking, budgeting
 
 # UI
 with st.sidebar:
@@ -45,8 +8,10 @@ with st.sidebar:
     page = st.radio('Navigate to', ['Home', 'About', 'Booking', 'Budgeting'])
 
 # Select page
-if page == 'Home': home()
-elif page == 'About': about()
-elif page == 'Booking': booking()
-elif page == 'Budgeting': budgeting()
-# else: home()
+page_info = {
+    'Home': home,
+    'About': about,
+    'Booking': booking,
+    'Budgeting': budgeting
+}
+page_info[page]()
