a01028941a3f7eeaf9b688a5ede217b3ce68cd6e
Lim JY <limjunyi03@gmail.com>
2023-06-10 09:59:27 +0000

    SImplify code

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 import streamlit as st
-from pages import home, about, booking, budgeting
+from pages import home, dist, booking, budgeting, about
+
+# Select page
+page_info = {
+    'Home': home,
+    'Airbnb Distributions': dist,
+    'Booking': booking,
+    'Budgeting': budgeting,
+    'About': about,
+}
 
 # UI
 with st.sidebar:
 
     st.title('Menu')
-    page = st.radio('Navigate to', ['Home', 'About', 'Booking', 'Budgeting'])
+    page = st.radio('Navigate to', page_info.keys())
 
-# Select page
-page_info = {
-    'Home': home,
-    'About': about,
-    'Booking': booking,
-    'Budgeting': budgeting
-}
 page_info[page]()
