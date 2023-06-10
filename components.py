9d2d3cde1921cdc86d09df386c645bad68af9a46
Lim JY <limjunyi03@gmail.com>
2023-06-10 09:58:58 +0000

    Added map

--- components.py
+++ components.py
@@ -11,11 +11,84 @@ def get_data():
     dataframe = pd.read_csv('./data/airbnb_data_clean.csv').iloc[:, 1:]
     return dataframe
 
+
+def get_states_dict() -> dict:
+    '''Map each city to state'''
+
+    return {
+        'Asheville': 'NC',
+        'Austin': 'TX',
+        'Boston': 'MA',
+        'Broward County': 'FL',
+        'Cambridge': 'MA',
+        'Chicago': 'IL',
+        'Clark County': 'NV',
+        'Columbus': 'OH',
+        'Denver': 'CO',
+        'Hawaii': 'HI',
+        'Jersey City': 'NJ',
+        'Los Angeles': 'SC',
+        'Nashville': 'TN',
+        'New Orleans': 'MS',
+        'New York City': 'NY',
+        'Oakland': 'CA',
+        'Pacific Grove': 'CA',
+        'Portland': 'OR',
+        'Rhode Island': 'RI',
+        'Salem': 'MA',
+        'San Clara Country': 'CA',
+        'Santa Cruz County': 'CA',
+        'San Diego': 'CA',
+        'San Francisco': 'CA',
+        'San Mateo County': 'CA',
+        'Seattle': 'WA',
+        'Twin Cities MSA': 'MN',
+        'Washington D.C.': 'DC',
+    }
+
+
+def get_lat_lon(city: str) -> tuple[float, float]:
+    '''Generate coordinate for each city'''
+
+    coordinate_dict = {
+        'Asheville': (35.5951, -82.5515),
+        'Austin': (30.2711, -97.7437),
+        'Boston': (42.3601, -71.0589),
+        'Broward County': (26.1901, -80.3659),
+        'Cambridge': (42.3736, -71.1097),
+        'Chicago': (41.8781, -87.6298),
+        'Clark County': (36.214, -115.013),
+        'Columbus': (39.9623, -83.0007),
+        'Denver': (39.7392, -104.9903),
+        'Hawaii': (20.7984, -156.3319),
+        'Jersey City': (40.7282, -74.0776),
+        'Los Angeles': (34.0522, -118.2437),
+        'Nashville': (36.1627, -86.7816),
+        'New Orleans': (29.9511, -90.0715),
+        'New York City': (40.7128, -74.006),
+        'Oakland': (37.8044, -122.2711),
+        'Pacific Grove': (36.6177, -121.9166),
+        'Portland': (45.5202, -122.6742),
+        'Rhode Island': (41.5801, -71.4774),
+        'Salem': (42.5195, -70.8967),
+        'San Clara Country': (37.3541, -121.9552),
+        'Santa Cruz County': (37.0454, -121.957),
+        'San Diego': (32.7157, -117.1611),
+        'San Francisco': (37.7749, -122.4194),
+        'San Mateo County': (37.4969, -122.3331),
+        'Seattle': (47.6062, -122.3321),
+        'Twin Cities MSA': (44.9778, -93.265),
+        'Washington D.C.': (38.9072, -77.0379)
+    }
+    return coordinate_dict[city]
+
+
 def header(heading: str):
     '''Format page header'''
 
     st.title(heading)
     st.markdown('---')
+
 
 def emptylines(num_of_lines: int) -> None:
     '''Create n empty lines'''
@@ -23,26 +96,31 @@ def emptylines(num_of_lines: int) -> None:
     for _ in range(num_of_lines):
         st.write('')
 
-def show_3d_map():
+
+def show_3d_map() -> None:
     '''Display 3D Map'''
 
-    map_data = pd.DataFrame()
-    map_data[['lat', 'lon']] = get_data()[['latitude', 'longitude']]
+    city = st.selectbox('Select a city', get_states_dict().keys())
+
+    map_data = get_data()
+    coords = map_data[map_data['city'] == get_states_dict()[city]][['latitude', 'longitude']]
+
+    latitude, longitude = get_lat_lon(city)
 
     st.pydeck_chart(pdk.Deck(
         map_style=None,
         initial_view_state=pdk.ViewState(
-            latitude=37.0902,
-            longitude=95.7129,
-            zoom=3,
+            latitude=latitude,
+            longitude=longitude,
+            zoom=11,
             pitch=50,
         ),
         layers=[
             pdk.Layer(
                 'HexagonLayer',
-                data=map_data,
-                get_position='[lon, lat]',
-                radius=2000,
+                data=coords,
+                get_position='[longitude, latitude]',
+                radius=200,
                 elevation_scale=4,
                 elevation_range=[0, 1000],
                 pickable=True,
@@ -50,96 +128,76 @@ def show_3d_map():
             ),
             pdk.Layer(
                 'ScatterplotLayer',
-                data=map_data,
-                get_position='[lon, lat]',
+                data=coords,
+                get_position='[longitude, latitude]',
                 get_color='[200, 30, 0, 160]',
-                get_radius=2000,
+                get_radius=200,
             ),
         ],
     ))
 
+    st.write('Height of bar represents the number of airbnb in that area')
+
+
 def get_state() -> str:
     '''Create state widget'''
 
-    states_dict = {
-        'Asheville':'NC',
-        'Austin':'TX',
-        'Boston':'MA',
-        'Broward County':'FL',
-        'Cambridge':'MA',
-        'Chicago':'IL',
-        'Clark County':'NV',
-        'Columbus':'OH',
-        'Denver':'CO',
-        'Hawaii':'HI',
-        'Jersey City':'NJ',    
-        'Los Angeles':'SC',
-        'Nashville':'TN',
-        'New Orleans':'MS',
-        'New York City':'NY',
-        'Oakland':'CA',
-        'Pacific Grove':'CA',
-        'Portland':'OR',
-        'Rhode Island':'RI',
-        'Salem':'MA',
-        'San Clara Country':'CA',
-        'Santa Cruz County':'CA',
-        'San Diego':'CA',
-        'San Francisco':'CA',
-        'San Mateo County':'CA',
-        'Seattle':'WA',
-        'Twin Cities MSA':'MN',
-        'Washington D.C.':'DC',
-    }
+    states_dict = get_states_dict()
 
-    city = st.selectbox('Select city:', ['Asheville', 'Austin', 'Boston', 'Broward County', 'Cambridge', 
-                                         'Chicago', 'Clark County', 'Columbus', 'Denver', 'Hawaii', 'Jersey City',
-                                         'Los Angeles', 'Nashville', 'New Orleans', 'New York City', 'Oakland',
-                                         'Pacific Grove', 'Portland', 'Rhode Island', 'Salem', 'San Clara Country',
-                                         'Santa Cruz County', 'San Diego', 'San Francisco', 'San Mateo County',
-                                         'Seattle', 'Twin Cities MSA', 'Washington D.C.'])
+    city = st.selectbox('Select city:', list(states_dict.keys()))
     return states_dict[city]
+
 
 def get_roomtype() -> str:
     '''Create room selection widget'''
 
-    room = st.selectbox('Select room type:', ['Shared room', 'Entire home/apt', 'Hotel room', 'Private room'])
+    room = st.selectbox('Select room type:', [
+                        'Shared room', 'Entire home/apt', 'Hotel room', 'Private room'])
     return room
 
-def searchbar() -> st.container():
+
+def searchbar() -> tuple[str]:
     '''Create a search bar'''
 
     with st.container():
 
         col1, col2 = st.columns([6, 2])
         search_bar = col1.text_input('Search a keyword:', placeholder='Search')
-        filter_by = col2.selectbox('Sort by:', ['Most Popular', 
-                                                'Price (Descending)', 'Price (Ascending)', 
+        filter_by = col2.selectbox('Sort by:', ['Most Popular',
+                                                'Price (Descending)', 'Price (Ascending)',
                                                 'Nights (Descending)', 'Nights (Ascending)'])
 
-        sort_by = {'by': ['price' if 'Price' in filter_by else \
-                          'minimum_nights' if 'Nights' in filter_by else 'number_of_reviews'], 
+        sort_by = {'by': ['price' if 'Price' in filter_by else
+                          'minimum_nights' if 'Nights' in filter_by else 'number_of_reviews'],
                    'ascending': 'Ascending' in filter_by}
 
     with st.container():
 
         col1, col2 = st.columns(2)
-        
-        with col1: city = get_state()
-        with col2: room = get_roomtype()
+
+        with col1:
+            city = get_state()
+        with col2:
+            room = get_roomtype()
 
     return search_bar, sort_by, city, room
 
+
 def display_result():
     '''Display query result in formatted layout'''
-    
-    df = get_data()
-    max_per_page=10
+
+    data = get_data()
+    max_per_page = 10
 
     search, sort_by, city, room = searchbar()
 
-    query_result = df[df.apply(lambda row: search in row['name_of_listing'], axis=1)].sort_values(**sort_by)
-    query_result = query_result[(query_result['city'] == city) & (query_result['room_type'] == room)]
+    query_result = data[
+        data.apply(lambda row: search in row['name_of_listing'], axis=1)
+    ].sort_values(**sort_by)
+
+    query_result = query_result[
+        (query_result['city'] == city) & (query_result['room_type'] == room)
+    ]
 
     no_result = len(query_result) == 0
 
@@ -148,14 +206,15 @@ def display_result():
     emptylines(3)
     st.write(f'Search result (Showing {0 if no_result else 1} - {upper_bound}) of {len(query_result)}')
 
-    if no_result: st.markdown('### No result')
-    else: 
+    if no_result:
+        st.markdown('### No result')
+    else:
         for i in range(min(max_per_page, len(query_result))):
 
             row = query_result.iloc[i]
 
             with st.container():
-                
+
                 col1, col2 = st.columns([1, 2])
                 col1.image(Image.open('./assets/img_placeholder.jpg'))
                 col2.markdown(f'''### {row['name_of_listing']}''')
@@ -165,27 +224,34 @@ def display_result():
                             ''')
                 col2.button('Book', key=f'book{i}')
 
+
 def input_prediction():
     '''Get user input for prediction'''
 
-    feature_col = ['predictions', 'number_of_reviews', 'CA', 'CO', 'DC', 'FL', 'HI', 'IL', 'MA', 'MN',
-                   'MS', 'NC', 'NJ','NV', 'NY', 'OH', 'OR', 'RI', 'SC', 'TN', 'TX',
+    feature_col = ['predictions', 'number_of_reviews', 'CA', 'CO', 'DC', 'FL', 'HI', 'IL', 'MA',
+                   'MN', 'MS', 'NC', 'NJ', 'NV', 'NY', 'OH', 'OR', 'RI', 'SC', 'TN', 'TX',
                    'Entire home/apt', 'Hotel room', 'Private room']
 
     user_input_arr = ['cozy'] + [0] * 23
-    
+
     col1, col2, col3 = st.columns([1, 2, 2])
 
-    user_input_arr[0] = st.text_input('Keyword in Airbnb:', placeholder='Enter a keyword')
-    user_input_arr[1] = col1.number_input('Number of reviews', min_value=0, value=1)
-    
-    with col2: city = get_state()
-    with col3: room = get_roomtype()
+    user_input_arr[0] = st.text_input(
+        'Keyword in Airbnb:', placeholder='Enter a keyword')
+    user_input_arr[1] = col1.number_input(
+        'Number of reviews', min_value=0, value=1)
 
-    if city != 'WA': user_input_arr[feature_col.index(city)] = 1
-    if room != 'Shared room': user_input_arr[feature_col.index(room)] = 1
+    with col2:
+        city = get_state()
+    with col3:
+        room = get_roomtype()
 
-    if st.button('Predict'): 
+    if city != 'WA':
+        user_input_arr[feature_col.index(city)] = 1
+    if room != 'Shared room':
+        user_input_arr[feature_col.index(room)] = 1
+
+    if st.button('Predict'):
         result = predict(user_input_arr)
         emptylines(3)
         st.write('The airbnb price of the configuration above is about')
